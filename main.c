657c4eef973cdc60d4d55ee19e2fab8e6fc5c473
HicaroD <hdanrlley1@gmail.com>
2022-07-29 16:46:28 -0300

    game: building main loop and setting windows to resizable

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <SDL2/SDL.h>
 
 #define WIDTH 720
 #define HEIGHT 480
 
 void check_sdl_error(int code) {
-    if(code < 0) {
+    if(code != 0) {
 	fprintf(stderr, "SDL ERROR: %s", SDL_GetError());
 	exit(EXIT_FAILURE);
     }
@@ -20,14 +21,33 @@ void check_sdl_pointer_error(void* ptr) {
 }
 
 int main() {
+    SDL_Window* screen;
+    SDL_Renderer* renderer;
+
     check_sdl_error(SDL_Init(SDL_INIT_VIDEO));
 
-    SDL_Window* screen = SDL_CreateWindow("Conway's Game of Life", 100, 100, WIDTH, HEIGHT, SDL_WINDOWEVENT_SHOWN);
+    screen = SDL_CreateWindow("Conway's Game of Life", 0, 0, WIDTH, HEIGHT, SDL_WINDOW_RESIZABLE);
     check_sdl_pointer_error(screen);
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED);
+    renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     check_sdl_pointer_error(renderer);
-    
+
+    bool running = true;
+    while(running) {
+	SDL_Event event;
+	while(SDL_PollEvent(&event)) {
+	    switch(event.type) {
+		case SDL_QUIT:
+		    running = false;
+		    break;
+	    }
+	}
+
+	SDL_SetRenderDrawColor(renderer, 0x0, 0x0, 0x0, 0x0);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+    }
+
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(screen);
     SDL_Quit();
