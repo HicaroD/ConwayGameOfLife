ed89b3c26a66d0ce1239c8dd2a6152bd200c3bf3
HicaroD <hdanrlley1@gmail.com>
2022-07-31 16:27:48 -0300

    game: refactoring neighbours counter and adding initial state from seed

--- game.c
+++ game.c
@@ -2,17 +2,12 @@
 #include <stdlib.h>
 #include "game.h"
 
-// FIXME: Is a malloc really necessary here? (performance issues because I'm doing over six millions malloc calls)
-struct Cell* new_cell(int x, int y) {
-    struct Cell* cell = (struct Cell*)malloc(sizeof(struct Cell));
-    cell->state = Dead;
-    cell->x = x;
-    cell->y = y;
+struct Cell new_cell(int x, int y) {
+    struct Cell cell = { Dead, x, y };
     return cell;
 }
 
-// TODO: Get initial state of the game (store coordinates on a text file)
-struct Game* new_game() {
+struct Game* new_game(size_t rows, int seed[rows][2]) {
     struct Game* game = (struct Game*)malloc(sizeof(struct Game));
 
     for(int i = 0; i < HEIGHT; i++) {
@@ -21,50 +16,59 @@ struct Game* new_game() {
 	}
     }
 
+    for(size_t row = 0; row < rows; row++) {
+	int x = seed[row][0];
+	int y = seed[row][1];
+	game->grid[y][x].state = Alive;
+    }
+
     return game;
 }
 
-SDL_Rect build_rect(int x, int y) {
-    SDL_Rect rectangle = { x, y, SQUARE_SIZE, SQUARE_SIZE};
-    return rectangle;
-}
-
-bool is_alive(struct Cell* cell) {
-    return cell->state == Alive;
+bool is_cell_on_edge(struct Cell* cell) {
+    int x = cell->x;
+    int y = cell->y;
+    return x == 0 || y == 0 || y + 1 >= HEIGHT || x + 1 >= WIDTH;
 }
 
 void update_game_board(struct Game* game) {
     for(int h = 0; h < HEIGHT; h++) {
 	for(int w = 0; w < WIDTH; w++) {
-	    // Check neibourghs of each cell and apply rules
-	    printf("%d\n", game->grid[h][w]->state);
+	    struct Cell current_cell = game->grid[h][w];
+
+	    if(!is_cell_on_edge(&current_cell)) {
+		int neighbours = count_cell_neighbors(game, &current_cell);
+
+		if(current_cell.state == Alive && (neighbours < 2 || neighbours > 3)) {
+		    printf("Kill cell\n");
+		    game->grid[h][w].state = Dead;
+		} else if(current_cell.state == Dead && neighbours == 3) {
+		    printf("Create cell\n");
+		    game->grid[h][w].state = Alive;
+		}
+	    }
 	}
     }
 }
 
 int count_cell_neighbors(struct Game* game, struct Cell* cell) {
     int sum = 0;
-    int x = cell->x;
-    int y = cell->y;
+    int x_axis = cell->x;
+    int y_axis = cell->y;
 
-    // TODO: Refactor this code (too verbose)
-    sum += (game->grid[y - 1 < 0 ? y : y - 1][x - 1 < 0 ? x : x - 1])->state;
-    sum += (game->grid[y - 1 < 0 ? y : y - 1][x])->state;
-    sum += (game->grid[y - 1 < 0 ? y : y - 1][x + 1 > WIDTH ? x : x - 1])->state;
+    for(int y = -1; y < 2; y++) {
+	for(int x = -1; x < 2; x++) {
+	    sum += (game->grid[y_axis + y][x_axis + x]).state;
+	}
+    }
 
-    sum += (game->grid[y][x - 1 < 0 ? x : x - 1])->state;
-    sum += (game->grid[y][x])->state;
-    sum += (game->grid[y][x + 1 > WIDTH ? x : x - 1])->state;
-
-    sum += (game->grid[y + 1 > HEIGHT ? y : y + 1][x - 1 < 0 ? x : x - 1])->state;
-    sum += (game->grid[y + 1 > HEIGHT ? y : y + 1][x])->state;
-    sum += (game->grid[y + 1 > HEIGHT ? y : y + 1][x + 1 > WIDTH ? x : x - 1])->state;
-
+    // The cell itself will not be counted as a neighbour
+    sum -= game->grid[y_axis][x_axis].state;
     return sum;
 }
 
-void set_cell_state_color(SDL_Renderer* renderer, struct Cell* cell) {
-	switch(cell->state) {
+void set_cell_state_color(SDL_Renderer* renderer, struct Cell cell) {
+	switch(cell.state) {
 	    case Alive:
 		SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 		break;
@@ -82,7 +86,7 @@ void draw_game_board(struct Game* game, SDL_Renderer* renderer) {
     for(int h = 0; h < HEIGHT; h++) {
 	for(int w = 0; w < WIDTH; w++) {
 	    set_cell_state_color(renderer, game->grid[h][w]);
-	    SDL_Rect rect = build_rect(w, h);
+	    SDL_Rect rect = { w * SCALE_FACTOR, h * SCALE_FACTOR, SCALE_FACTOR, SCALE_FACTOR};
 	    SDL_RenderFillRect(renderer, &rect);
 	}
     }
