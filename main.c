8f7ef6062b37e30bae7f68b99d52a6241ed9330c
HicaroD <hdanrlley1@gmail.com>
2022-07-31 16:29:09 -0300

    main: changing screen size and implementing scale factor for cell size

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 
 #include "game.h"
 
+#define SEED_ROWS 1
+
 void check_sdl_error(int code) {
     if(code != 0) {
 	fprintf(stderr, "SDL ERROR: %s", SDL_GetError());
@@ -19,19 +21,32 @@ void check_sdl_pointer_error(void* ptr) {
     }
 }
 
+/* int** generate_random_seed() { */
+/*     for(int row = 0; row < SEED_ROWS; row++) { */
+/* 	int x = rand() % WIDTH; */
+/* 	int y = rand() % HEIGHT; */
+
+/* 	seed[row][0] = y; */
+/* 	seed[row][1] = x; */
+/*     } */
+
+/*     return seed; */
+/* } */
+
 int main() {
     SDL_Window* screen;
     SDL_Renderer* renderer;
 
     check_sdl_error(SDL_Init(SDL_INIT_VIDEO));
 
-    screen = SDL_CreateWindow("Conway's Game of Life", 0, 0, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
+    screen = SDL_CreateWindow("Conway's Game of Life", 0, 0, SCREEN_WIDTH, SCREEN_WIDTH, SDL_WINDOW_SHOWN);
     check_sdl_pointer_error(screen);
 
     renderer = SDL_CreateRenderer(screen, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     check_sdl_pointer_error(renderer);
 
-    struct Game* game = new_game();
+    int seed[SEED_ROWS][2] = {{10, 10}};
+    struct Game* game = new_game(SEED_ROWS, seed);
     bool running = true;
 
     while(running) {
@@ -49,6 +64,8 @@ int main() {
 	draw_game_board(game, renderer);
 	SDL_RenderPresent(renderer);
     }
+
+    free(game);
 
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(screen);
